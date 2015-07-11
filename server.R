16ef700f15dd7cb0e86232eedfdd8b8bde2cadde
josersosa <josersosa@gmail.com>
2015-07-11 21:06:10 +0000

    Mostrar hipotesis alternativa

--- server.R
+++ server.R
@@ -31,6 +31,7 @@ shinyServer(function(input, output) {
     pc <- qnorm(1-alpha, mean = mH0, sd = sH0)
     err1 <- 1 -pnorm(pc, mean = mH0, sd = sH0)
     err2 <- pnorm(pc, mean = mH1, sd = sH1)
+    mostrarH1 <- input$MostrarH1
     
     par(mfrow = c(1,1))
     
@@ -42,37 +43,43 @@ shinyServer(function(input, output) {
          xlim = xlim, ylim = ylim, 
          col = "blue", 
          lwd = 2)
-    
-    curve(dnorm(x, mean = mH1, sd = sH1), 
-          xlim[1] , xlim[2],
-          col = "red", 
-          lwd = 2, 
-          add = TRUE)
-    
     lines(c(mH0, mH0), 
           c(0, dnorm(mH0, mean = mH0, sd = sH0)), 
           col="blue", lwd = 1, lty = "dashed")
-    lines(c(mH1, mH1), 
-          c(0, dnorm(mH1, mean = mH1, sd = sH1)), 
-          col="red", lwd = 1, lty = "dashed")
     
-    mtext("Hipótesis Nula verdadera", col="blue", adj = 0)
-    mtext("Hipótesis Alternativa verdadera", col = "red", adj = 1)
+    if(mostrarH1){
+      curve(dnorm(x, mean = mH1, sd = sH1), 
+            xlim[1] , xlim[2],
+            col = "red", 
+            lwd = 2, 
+            add = TRUE)
+      lines(c(mH1, mH1), 
+            c(0, dnorm(mH1, mean = mH1, sd = sH1)), 
+            col="red", lwd = 1, lty = "dashed")
+      mtext("Hipótesis Alternativa verdadera", col = "red", adj = 1)
+      text(xlim[2]-2, ylim[2]-0.04, 
+           as.expression(substitute(m1==medy,list(medy=mH1))), col="red")
+      text(xlim[2]-2, ylim[2]-0.12, 
+           as.expression(substitute(beta==e2,list(e2=err2))), col="red")
+      text(mH1, ylim[1]-0.01, "m1")
+      x <- seq(xlim[1], pc, length.out = 50)
+      y <- dnorm(x, mean = mH1, sd = sH1)
+      x <- c(x[1],x,x[50])
+      y <- c(0,y,0)
+      polygon(x, y, density = 20, angle = -45, col = "red")
+    }   
+    
+    mtext("Hipótesis Nula verdadera", col="blue", adj = 0)    
     text(xlim[2]-2, ylim[2]-0.0, 
-         as.expression(substitute(m0==medx,list(medx=mH0))), col="blue")
-    text(xlim[2]-2, ylim[2]-0.04, 
-         as.expression(substitute(m1==medy,list(medy=mH1))), col="red")
+         as.expression(substitute(m0==medx,list(medx=mH0))), col="blue")    
     text(xlim[2]-2, ylim[2]-0.08, 
-         as.expression(substitute(alpha==e1,list(e1=err1))), col="blue")
-    text(xlim[2]-2, ylim[2]-0.12, 
-         as.expression(substitute(beta==e2,list(e2=err2))), col="red")
+         as.expression(substitute(alpha==e1,list(e1=err1))), col="blue")    
     text(xlim[2]-2, ylim[2]-0.16, 
          as.expression(substitute(pc==k,list(k=pc))))
     text(xlim[2]-2, ylim[2]-0.2, 
          as.expression(substitute(n==tam,list(tam=n))))
     
     text(mH0, ylim[1]-0.01, "m0")
-    text(mH1, ylim[1]-0.01, "m1")
     text(pc, ylim[1]-0.01, "pc")
     
     x <- seq(pc, xlim[2], length.out = 50)
@@ -81,14 +88,7 @@ shinyServer(function(input, output) {
     y <- c(0,y,0)
     polygon(x, y, density = 20, angle = 45, col = "blue")
     #polygon(x, y, col = "light blue")
-    
-    x <- seq(xlim[1], pc, length.out = 50)
-    y <- dnorm(x, mean = mH1, sd = sH1)
-    x <- c(x[1],x,x[50])
-    y <- c(0,y,0)
-    polygon(x, y, density = 20, angle = -45, col = "red")
-    
-    withMathJax(helpText('Dynamic output 1:  $$\\alpha^2$$'))
+
   })
   
   # Gráfico de la potencia de la prueba
--- ui.R
+++ ui.R
@@ -14,18 +14,20 @@ shinyUI(fluidPage(withMathJax(),
                   min = 10,
                   max = 150,
                   value = 50),
+      sliderInput("alpha",
+                  "Significancia (alpha):",
+                  min = 0.01,
+                  max = 0.5,
+                  value = 0.05,
+                  step= 0.005),
       sliderInput("mH1",
                   "Media, hipótesis Alternativa (mu_1):",
                   min = 4,
                   max = 14,
                   value = 8,
                   step= 0.2),
-      sliderInput("alpha",
-                  "Significancia (alpha):",
-                  min = 0.01,
-                  max = 0.5,
-                  value = 0.05,
-                  step= 0.005)
+      checkboxInput('MostrarH1', 'Mostrar hipótesis Alternativa', TRUE)
+      
     ),
 
     # Paneles con las salidas
