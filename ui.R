e8efd57bcb039b189d7de6ee838c10304a929695
josersosa <josersosa@gmail.com>
2015-07-12 07:48:48 +0000

    Comentarios en la barra de controles

--- ui.R
+++ ui.R
@@ -8,18 +8,21 @@ shinyUI(fluidPage(withMathJax(),
   
   # Barra lateral con los controles
   sidebarLayout(position = "right",
-    sidebarPanel(
+    sidebarPanel( withMathJax(
+      helpText("El estadístico de prueba usado (la media muestral) se calculará sobre una muestra (evidencia estadística) de tamaño n."),
       sliderInput("n",
                   "Tamaño de la muestra (n):",
                   min = 10,
                   max = 150,
                   value = 50),
+      helpText("El nivel de confianza de la prueba (\\(1-\\alpha\\)) suele ser alto, entre un 90% y un 99%. La significancia es el complemeno de este valor (\\(\\alpha\\).)"),
       sliderInput("alpha",
                   "Significancia (alpha):",
                   min = 0.01,
                   max = 0.5,
                   value = 0.05,
                   step= 0.005),
+      helpText("La hipótesis alternativa contradice la hipótesis nula, proponiedo un nuevo valor para el parámetro de interes (\\(\\mu\\))."),
       sliderInput("mH1",
                   "Media, hipótesis Alternativa (mu_1):",
                   min = 4,
@@ -28,7 +31,7 @@ shinyUI(fluidPage(withMathJax(),
                   step= 0.2),
       checkboxInput('MostrarH1', 'Mostrar hipótesis Alternativa', TRUE)
       
-    ),
+    )),
 
     # Paneles con las salidas
     mainPanel(
